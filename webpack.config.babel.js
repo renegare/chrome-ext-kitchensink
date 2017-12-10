dff7a14018a34e2713f15e76c7e4a41996a0fb7e
Mudi <mudi@renegare.com>
2017-12-10 10:41:21 +0000

    add webpack and react to extension

---
+++ webpack.config.babel.js
@@ -0,0 +1,37 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+
+export default {
+  entry: {
+    popup: './ext/popup.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, './ext')],
+        use: 'babel-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    // create popup.html from template and inject styles and script bundles
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ['popup'],
+      filename: 'popup.html',
+      template: './ext/popup.html',
+    }),
+    // copy extension manifest and icons
+    new CopyWebpackPlugin([{ from: './ext/manifest.json' }]),
+    new CopyWebpackPlugin([{ from: './ext/icon.png' }]),
+  ],
+}
